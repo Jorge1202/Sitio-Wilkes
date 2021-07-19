cb03b1082de0df09a264b55c2bbe6cb1086bd159
Jorge1202 <yorch.uaeh@gmail.com>
2021-07-19 12:21:06 -0500

    first commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../images/logo.svg';
+import './styles/App.css';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jorge <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn jorge
+
         </a>
       </header>
     </div>
